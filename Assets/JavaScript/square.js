3c123fb18f8437d184012cb185e607df26f08bbe
WillRMorris <153463826+WillRMorris@users.noreply.github.com>
2024-02-01 21:15:48 -0700

    started functions for checking squares, made function to get coordinates of a square in all numbers. touched up square class and changed return value of generateBoard

--- Assets/JavaScript/square.js
+++ Assets/JavaScript/square.js
@@ -1,15 +1,21 @@
 
 class Square{
-    constructor(id, occupation, enPassant){
-        
+    constructor(id, occupation, enPassant, el){
+        this.id = id;
+        this.occupation = occupation;
+        this.enPassant = enPassant;
+        this.el = el;
     }
 }
 
 function generateBoard () {
     var squares = document.getElementsByClassName('spaces');
+    var board =[];
     for (var i = 0; i <squares.length; i++){
         var target = squares[i];
         var id  = target.getAttribute('id');
-        var create = new Square(id, '0', false);
+        var create = new Square(id, '0', false, target);
+        board.push(create);
     };
+    return board;
 }
